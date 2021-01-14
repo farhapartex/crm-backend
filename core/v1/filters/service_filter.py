3a2056ddd4bdec79183a253bc7b220ccc9feee4f
Md Nazmul Hasan <farhapartex@gmail.com>
2021-01-14 22:55:55 +0600

    latest

--- core/v1/filters/service_filter.py
+++ core/v1/filters/service_filter.py
@@ -11,6 +11,7 @@ class ServiceFilter(filters.FilterSet):
     service_type_id = filters.CharFilter(method="filter_by_service_type_id")
     service_type_name = filters.CharFilter(method="filter_by_service_type_name")
     generic_search = filters.CharFilter(method="filter_by_generic_search")
+    is_active = filters.BooleanFilter(method="filter_by_is_active")
 
     def filter_by_uid(self, queryset, name, value):
         if value is None:
@@ -49,5 +50,13 @@ class ServiceFilter(filters.FilterSet):
             return queryset
         try:
             return queryset.filter(Q(service_type__name__contains=value.capitalize()) | Q(name__contains=value) | Q(uid__contains=value))
+        except:
+            return queryset.none()
+
+    def filter_by_is_active(self, queryset, name, value):
+        if value is None:
+            return queryset
+        try:
+            return queryset.filter(is_active=value)
         except:
             return queryset.none()
